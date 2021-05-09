239972960daebc50f0db6726ece24848ce2e9096
LukaNestorovic <lukanestorovic41@gmail.com>
2021-05-10 01:01:21 +0200

    Inic

---
+++ src/main/java/com/example/WebProg/WebProgApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebProg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProgApplication.class, args);
+	}
+
+}
