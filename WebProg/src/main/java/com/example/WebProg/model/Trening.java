40cefca46112a7dde2e1c08711a7572b52cab68a
LukaNestorovic <lukanestorovic41@gmail.com>
2021-05-10 11:30:17 +0200

    a

--- WebProg/src/main/java/com/example/WebProg/model/Trening.java
+++ WebProg/src/main/java/com/example/WebProg/model/Trening.java
@@ -23,4 +23,44 @@ public class Trening implements Serializable {
 
     @Column
     private Integer trajanje;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public String getTip_treninga() {
+        return tip_treninga;
+    }
+
+    public void setTip_treninga(String tip_treninga) {
+        this.tip_treninga = tip_treninga;
+    }
+
+    public Integer getTrajanje() {
+        return trajanje;
+    }
+
+    public void setTrajanje(Integer trajanje) {
+        this.trajanje = trajanje;
+    }
 }
