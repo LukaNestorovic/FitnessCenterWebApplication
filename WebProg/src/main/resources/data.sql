ddecd439a1cd6ce38de7c056ec010c3a28166cb4
LukaNestorovic <lukanestorovic41@gmail.com>
2021-05-13 16:48:02 +0200

    Update data.sql

--- WebProg/src/main/resources/data.sql
+++ WebProg/src/main/resources/data.sql
@@ -5,11 +5,12 @@ Insert into Trener (korisnicko_ime,lozinka,ime,prezime,kontakt_telefon,email,dat
 Insert into Clan (korisnicko_ime,lozinka,ime,prezime,kontakt_telefon,email,datum_rodjenja,uloga,aktivan) values ('Luka123','123123','Luka','Nestorovic','065444','luka@gmail.com','2001-02-12','Clan',true);
 Insert into Clan (korisnicko_ime,lozinka,ime,prezime,kontakt_telefon,email,datum_rodjenja,uloga,aktivan) values ('Luka12','12312','Luka','Nestorovic','06544445','luka12@gmail.com','2002-02-12','Clan',true);
 
-
-Insert into Termin (cena,datum_vreme) values (500,'2021-05-01');
-
 Insert into Trening (naziv,opis,tip_treninga,trajanje) values ('Kardio','Trcanje','Kardio',2);
 
 Insert into Fitnes_Centar (naziv,adresa,email,broj_telefona) values ('FitnesCentar','Drinske divizije bb','fitnes@gmail.com','05452');
 
 Insert into Sala (kapacitet,oznaka_sale) values (200,'101');
+
+Insert into Termin (cena,datum_vreme) values (500,'2021-05-01');
+
+Insert into Odradjeni_Trening (ocena) values (5);
