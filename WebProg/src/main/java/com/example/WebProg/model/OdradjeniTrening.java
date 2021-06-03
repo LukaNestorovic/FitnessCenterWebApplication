60e6f7e4c8cbc09d1e8a0b6dbebaea9c05a429da
LukaNestorovic <lukanestorovic41@gmail.com>
2021-06-04 00:34:35 +0200

    A

--- WebProg/src/main/java/com/example/WebProg/model/OdradjeniTrening.java
+++ WebProg/src/main/java/com/example/WebProg/model/OdradjeniTrening.java
@@ -2,8 +2,6 @@ package com.example.WebProg.model;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 public class OdradjeniTrening implements Serializable {
--- WebProg/src/main/resources/data.sql
+++ WebProg/src/main/resources/data.sql
@@ -1,16 +1,16 @@
 Insert into Administrator (korisnicko_ime,lozinka,ime,prezime,kontakt_telefon,email,datum_rodjenja,uloga,aktivan) values ('Luka123','123123','Luka','Nestorovic','065444','luka@gmail.com','2001-02-12','Administrator',true);
 
-Insert into Trener (korisnicko_ime,lozinka,ime,prezime,kontakt_telefon,email,datum_rodjenja,uloga,aktivan) values ('Luka123','123123','Luka','Nestorovic','065444','luka@gmail.com','2001-02-12','Trener',true);
-
 Insert into Clan (korisnicko_ime,lozinka,ime,prezime,kontakt_telefon,email,datum_rodjenja,uloga,aktivan) values ('Luka123','123123','Luka','Nestorovic','065444','luka@gmail.com','2001-02-12','Clan',true);
 Insert into Clan (korisnicko_ime,lozinka,ime,prezime,kontakt_telefon,email,datum_rodjenja,uloga,aktivan) values ('Luka12','12312','Luka','Nestorovic','06544445','luka12@gmail.com','2002-02-12','Clan',true);
 
-Insert into Trening (naziv,opis,tip_treninga,trajanje) values ('Kardio','Trcanje','Kardio',2);
-
 Insert into Fitnes_Centar (naziv,adresa,email,broj_telefona) values ('FitnesCentar','Drinske divizije bb','fitnes@gmail.com','05452');
 
-Insert into Sala (kapacitet,oznaka_sale) values (200,'101');
+Insert into Trener (korisnicko_ime,lozinka,ime,prezime,kontakt_telefon,email,datum_rodjenja,uloga,aktivan,prosecna_ocena,fitnes_centar_id) values ('Luka123','123123','Luka','Nestorovic','065444','luka@gmail.com','2001-02-12','Trener',true,5,1);
 
-Insert into Termin (cena,datum_vreme) values (500,'2021-05-01');
+Insert into Trening (naziv,opis,tip_treninga,trajanje,trener_id) values ('Kardio','Trcanje','Kardio',2,1);
 
-Insert into Odradjeni_Trening (ocena) values (5);
+Insert into Sala (kapacitet,oznaka_sale,fitnes_centar_id) values (200,'101',1);
+
+Insert into Termin (cena,datum_vreme,fitnes_centar_id,trening_id,odradjeni_trening_id,ocena_id,clan_id,sala_id) values (500,'2021-05-01',1,1,1,1,1,1);
+
+Insert into Odradjeni_Trening (ocena,trening_id,termin_id) values (5,1,1);
